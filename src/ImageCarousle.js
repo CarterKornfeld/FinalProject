67c4c6f499f388fbff84bf0eb0116c1046bbd71f
CarterKornfeld <148253614+CarterKornfeld@users.noreply.github.com>
2024-04-09 15:02:42 -0400

    ads

--- src/ImageCarousle.js
+++ src/ImageCarousle.js
@@ -1,11 +1,6 @@
 
 import { LitElement, html, css } from 'lit';
 
-/**
- * Now it's your turn. Here's what we need to try and do:
- * 1. Get you HTML from your card working in here 
- * 2. Get your CSS rescoped as needed to work here
- */
 
 export class ImageCarousle extends LitElement {
 
@@ -26,23 +21,97 @@ export class ImageCarousle extends LitElement {
 
       .backdrop
       {
+        margin: 8px;
+        padding: 8px;
         width:500px;
         height:500px;
-        background-color:blue;
+        background-color:green;
         color: black;
+        position: relative;
           }
+
+        .topRow
+          {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            
+          }
+        .currentNum
+        {
+            margin: 8px;
+            display: flex;
+            justify-content: left;
+        }
+        .closeButton
+        {
+        margin: 8px;
+        
+        display: flex;
+            justify-content: right
+        }
+        .description
+        {
+            align-self: center;
+            display: flex;
+            justify-content: center;
+        }
+        .displayedImage
+        {
+            width: 250px;
+            height: 250px;
+        }
+
+        .imageLoop
+        {
+            margin: 8px;
+            display: flex;
+            justify-content: space-between;  
+            position: absolute;
+           bottom: 0;
+           width: 95%;
+        }
+
     `;
   }
 
   render() {
     return html`
     <div class="backdrop">
-        <div class=body>
-        <div id=text1> askfhaslfhldifhkalchaoshlfasifhkalschalshlishalsfhalsfhlasf.halhsf</div>
-        <media-image></media-image>
-        </div>
-    
 
+    <div class="topRow">
+
+    <div class="currentNum">
+    1 of 3
+    </div>
+
+    <div class="description">
+    Cool Pic
+    </div>
+
+    <div class="closeButton">
+    X
+</div>
+
+</div>
+
+
+
+    <div class="displayedImage">
+    {}
+    </div>
+
+    <div class="imageLoop">
+        <div class="leftButton">
+        <-
+        </div>
+        <div class = "imageThumbNails">
+        [][][][]
+        </div>
+        <div class = "rightButton">
+            ->
+        </div>
+    </div>
 
     </div>
     
--- src/media-image.js
+++ src/media-image.js
@@ -1,1 +1,39 @@
 
+import { LitElement, html, css } from 'lit';
+
+
+export class mediaImage extends LitElement {
+
+  static get tag() {
+    return 'media-image';
+  }
+
+  constructor() {
+    super();
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+
+    `;
+  }
+
+  render() {
+    return html`
+    
+    
+    `;
+  }
+
+  static get properties() {
+    return {
+      
+    };
+  }
+}
+
+globalThis.customElements.define(mediaImage.tag, mediaImage);
+
