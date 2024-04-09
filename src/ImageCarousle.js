01fecd4c3fd3c90b089337aed63eed132f34c0fd
CarterKornfeld <148253614+CarterKornfeld@users.noreply.github.com>
2024-04-09 14:01:06 -0400

    s

--- src/ImageCarousle.js
+++ src/ImageCarousle.js
@@ -1,1 +1,55 @@
 
+import { LitElement, html, css } from 'lit';
+
+/**
+ * Now it's your turn. Here's what we need to try and do:
+ * 1. Get you HTML from your card working in here 
+ * 2. Get your CSS rescoped as needed to work here
+ */
+
+export class ImageCarousle extends LitElement {
+
+  static get tag() {
+    return 'image-carousle';
+  }
+
+  constructor() {
+    super();
+    this.title = "image-carousle";
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+
+      .backdrop
+      {
+        width:500px;
+        height:500px;
+        background-color:blue;
+        color: black;
+          }
+    `;
+  }
+
+  render() {
+    return html`
+    <div class="backdrop">
+    
+
+
+    </div>
+    
+    `;
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+    };
+  }
+}
+
+globalThis.customElements.define(ImageCarousle.tag, ImageCarousle);
